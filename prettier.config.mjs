02d9388db862a8456d6545dfe1872f9ada9018eb
Leonardo <leonardo.yakub@gmail.com>
2024-07-27 18:30:39 +0200

    set prettier config to ES6 module file

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: [
-    "prettier-plugin-tailwindcss",
     "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
   ],
   importOrder: ["^react(.*)$", "<THIRD_PARTY_MODULES>", "^[./]|@/(.*)$"],
   importOrderSeparation: true,
